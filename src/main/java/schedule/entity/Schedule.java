7277a8ade81f2943b1426f32814bfefad8c4688a
minss22 <kms141516@gmail.com>
2025-11-04 14:31:22 +0900

    ✨ feat: Entity 구현 - 일정 데이터
    - Schedule 클래스: 고유 식별자(ID), 일정 제목, 일정 내용, 작성자명, 비밀번호 저장
    - BaseEntity 클래스: JPA Auditing을 활용하여 작성일, 수정일을 저장

---
+++ src/main/java/schedule/entity/Schedule.java
@@ -0,0 +1,38 @@
+package schedule.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "schedules")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 50, nullable = false)
+    private String title; // 일정 제목
+    @Column(nullable = false)
+    private String content; // 일정 내용
+    @Column(length = 50, nullable = false)
+    private String name; // 작성자
+    @Column(length = 50, nullable = false)
+    private String password; // 비밀번호
+
+    public Schedule(String title, String content, String name, String password) {
+        this.title = title;
+        this.content = content;
+        this.name = name;
+        this.password = password;
+    }
+
+    public void update(String title, String content, String name, String password) {
+        this.title = title;
+        this.content = content;
+        this.name = name;
+        this.password = password;
+    }
+}
