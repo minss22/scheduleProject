4219f9fe5521510fe8a541963024ccafbc3194e3
minss22 <kms141516@gmail.com>
2025-11-04 20:53:43 +0900

    ✨ feat: Lv 1. 일정 생성 - Service, DTO 클래스 작성
    - CreateScheduleRequest 클래스:
      - 요청받은 데이터를 담기위한 DTO 클래스
      - id를 제외한 나머지 속성 정의
    - CreateScheduleResponse 클래스:
      - 응답할 데이터를 담은 DTO 클래스
      - 비밀번호를 제외한 나머지 속성과 작성일, 수정일을 final로 정의
    - ScheduleService 클래스:
      - DTO로 받아온 데이터를 Schedule로 바꾸어 데이터베이스에 저장 후, 다시 DTO로 바꾸어 리턴

---
+++ src/main/java/schedule/dto/CreateScheduleRequest.java
@@ -0,0 +1,11 @@
+package schedule.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CreateScheduleRequest {
+    private String title;
+    private String content;
+    private String name;
+    private String password;
+}
--- src/main/java/schedule/service/ScheduleService.java
+++ src/main/java/schedule/service/ScheduleService.java
@@ -2,10 +2,39 @@ package schedule.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import schedule.dto.CreateScheduleRequest;
+import schedule.dto.CreateScheduleResponse;
+import schedule.dto.GetOneScheduleResponse;
+import schedule.entity.Schedule;
 import schedule.repository.ScheduleRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
+
+    // Lv 1. 일정 생성
+    @Transactional
+    public CreateScheduleResponse save(CreateScheduleRequest request) {
+        Schedule schedule = new Schedule(
+                request.getTitle(),
+                request.getContent(),
+                request.getName(),
+                request.getPassword()
+        );
+
+        Schedule saved = scheduleRepository.save(schedule);
+        return new CreateScheduleResponse(
+                saved.getId(),
+                saved.getTitle(),
+                saved.getContent(),
+                saved.getName(),
+                saved.getCreatedAt(),
+                saved.getModifiedAt()
+        );
+    }
 }
