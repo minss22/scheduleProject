5b56ad3ed535b2c4c590c0ddc585d537d91ae171
minss22 <kms141516@gmail.com>
2025-11-04 10:37:40 +0900

    Initial commit

---
+++ src/main/java/schedule/ScheduleProjectApplication.java
@@ -0,0 +1,13 @@
+package schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleProjectApplication.class, args);
+    }
+
+}
