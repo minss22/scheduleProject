7e03783e0abd4c2d2af202489af6a9106528c05f
minss22 <kms141516@gmail.com>
2025-11-05 10:14:49 +0900

    ✨ feat: Controller 클래스 로직 작성
    - Service의 메서드 실행하여 일정을 생성/조회/수정/삭제함
    - ResponseEntity 사용

--- src/main/java/schedule/controller/ScheduleController.java
+++ src/main/java/schedule/controller/ScheduleController.java
@@ -1,11 +1,46 @@
 package schedule.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import schedule.dto.*;
 import schedule.service.ScheduleService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ScheduleController {
     private final ScheduleService scheduleService;
+
+    @PostMapping("/schedules")
+    public ResponseEntity<ScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
+        ScheduleResponse result = scheduleService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @GetMapping("/schedules")
+    public ResponseEntity<List<ScheduleResponse>> getAllSchedule(@RequestParam(required = false) String name) {
+        List<ScheduleResponse> result = scheduleService.getAll(name);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping("/schedules/{userId}")
+    public ResponseEntity<ScheduleResponse> getOneSchedule(@PathVariable Long userId) {
+        ScheduleResponse result = scheduleService.getOne(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @PutMapping("/schedules/{userId}")
+    public ResponseEntity<ScheduleResponse> updateSchedule(@PathVariable Long userId, @RequestBody UpdateScheduleRequest request) {
+        ScheduleResponse result = scheduleService.update(userId, request);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @DeleteMapping("/schedules/{userId}")
+    public ResponseEntity<List<Void>> deleteSchedule(@PathVariable Long userId, @RequestBody DeleteScheduleRequest request) {
+        scheduleService.delete(userId, request);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
